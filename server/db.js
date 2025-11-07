50abbed795a44792002754ffbb4a64964f35a713
srimannath-5034 <siripuram.2024@vitstudent.ac.in>
2025-11-08 01:32:47 +0530

    Create db.js

---
+++ server/db.js
@@ -0,0 +1,15 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, '../database.sqlite');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Database connection failed:", err.message);
+    } else {
+        console.log("Connected to SQLite database");
+    }
+});
+
+module.exports = db;
